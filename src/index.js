7de9e5077a4d81085b127c4166d560fa0a905840
trbenne2 <luvsrub@Tylers-MacBook-Pro-2.local>
2020-04-19 16:39:59 -0400

    added auth and routing

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from './constants';
 
 // 1
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom';
 
 // 2
 const httpLink = createHttpLink({
@@ -16,16 +19,29 @@ const httpLink = createHttpLink({
 });
 
 // 3
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 // 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 serviceWorker.unregister();
